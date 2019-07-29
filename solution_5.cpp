39110c7e58883e2f1731b5f3629b75201179b18a
Desirelife <897644126@qq.com>
2019-07-29 18:37:53 +0800

    进制转换添加小数情况

--- solution_5.cpp
+++ solution_5.cpp
@@ -1,13 +1,57 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+
 using namespace std;
 
-void binaryConvert();
+int integerBinaryConvert(string binaryString);
+float decimalBinaryConvert(string binaryString);
+float binaryConvert(string binaryString);
 
 int main(){
-
-
+    string binaryString;
+    cin >> binaryString;
+    float result = binaryConvert(binaryString);
+    cout << result;
 }
 
-void binaryConvert(){
+int integerBinaryConvert(string integerBinaryString){
+    int n = integerBinaryString.length();
+    int weights = 1;
+    int intResult = 0;
+    while(n--) {
+        intResult += (integerBinaryString[n]-'0') * weights;
+        weights *= 2;
+    }
+    return intResult;
+}
 
+float decimalBinaryConvert(string decimalBinaryString){
+    int n = decimalBinaryString.length();
+    float weights = 0.5;
+    float decResult = 0;
+    for(int i = 0; i < n; i++){
+        decResult += (decimalBinaryString[i] - '0') * weights;
+        weights *= 0.5;
+    }
+    return decResult;
+}
+
+float binaryConvert(string binaryString) {
+    string integerBinaryString;
+    string decimalBinaryString;
+    int n = binaryString.length();
+    int pos = binaryString.find('.');
+    if(pos == -1){
+        integerBinaryString = binaryString;
+        decimalBinaryString = "0";
+    } else{
+        for(int i = 0; i < n; i++){
+            if(binaryString[i] == '.'){
+                integerBinaryString = binaryString.substr(0, i);
+                decimalBinaryString = binaryString.substr(i + 1, n);
+            }
+        }
+    }
+    return integerBinaryConvert(integerBinaryString) + decimalBinaryConvert(decimalBinaryString);
 }
